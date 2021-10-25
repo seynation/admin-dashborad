58747cbdcb203304c2d5f83692e590fef6f4d199
Omega Thomson <82154373+seynation@users.noreply.github.com>
2021-10-25 07:23:41 +0300

    Add files via upload

---
+++ my_chart.js
@@ -0,0 +1,49 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var earning = document.getElementById('earning').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['Facebook', 'YouTube', 'Yellow', 'Amazon'],
+        datasets: [{
+            label: 'Traffic Sources',
+            data: [1200, 1900, 3000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
+
+var myChart = new Chart(earning, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August' , 
+        'September', 'October', 'November', 'December'],
+        datasets: [{
+            label: 'Earning',
+            data: [12000, 19344, 3934, 5048, 24974, 3985, 8894, 7125, 8893, 9943, 12894, 1594,1448],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1 )',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1 )',
+            ],
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
